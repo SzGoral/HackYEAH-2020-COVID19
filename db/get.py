d57a84a1e812fa5e781390bca9653dc71042fe12
Michał Bizoń <michal@Michas-MacBook-Pro.local>
2020-04-04 00:47:28 +0200

    first db draft

--- db/get.py
+++ db/get.py
@@ -1,13 +1,24 @@
 import json
 
-from constant import DATABASE_FILE
+from constants import DATABASE_FILE
 
-# and from package core we call
-# from db import get
-# now we don care about how the get function get the data from anywhere
-def get():
+
+def get_all_day(year, month, day):
+    year, month, day = [str(i) for i in [year, month, day]]
     with open(DATABASE_FILE, "r") as db:
         data = json.load(db)
-        return data[0]["long"]
+        return data[year][month][day]
+
+def get(year, month, day, hour=None):
+    all_day = get_all_day(year, month, day)
+    if hour is not None:
+        return all_day[str(hour)]
+    return all_day
 
 
+
+if __name__ == "__main__":
+    x = get(2020, 4, 3, 21)
+    print(x)
+    x = get(2020, 4, 3)
+    print(x)
